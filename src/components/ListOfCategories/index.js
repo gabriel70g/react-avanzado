e0ab159305b0d3f4de20854395080cc4da085095
Gabriel Gomez <gabriel70@gmail.com>
2020-09-28 22:57:06 -0300

    cambios en Categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,23 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../category'
+import { List, Item } from './styles'
 
-import { List, Item  } from './styles'
-import { categories as mockCategories } from '../../../api/db.json'
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
-export const ListOfCategories = () =>{
-  const [categories, setCategories] = useState(mockCategories)
+  useEffect(function () {
+    setLoading(true)
+    window.fetch('https://petgram-server-gabys-nomreb8hx.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
 
-  useEffect(function(){
-    
-  })
+  return { categories, loading }
+}
 
-  return (
-    <List>
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        categories.map(categ => <Item key={ categ.id } ><Category {...categ} /></Item>)
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  )
 }
-
-
