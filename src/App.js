ef3a041c8e38f80ad3d5dd5e465fe061f7fd0270
Gabriel Gomez <gabriel70@gmail.com>
2020-10-10 19:48:14 -0300

    Rutas terminadas

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
-import React from "react";
+import React, {useContext} from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
 import { NavBar } from "./components/NavBar";
-
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
-
-import { Router } from "@reach/router";
-import Context from "./Context";
+import { Redirect, Router } from "@reach/router";
+import { Context } from "./Context";
+import { NotFound } from './pages/NotFound';
+import { from } from "apollo-boost";
 
 export const App = () => {
+
+const { isAuth } = useContext(Context)
+
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound  default />
         <Home path="/" />
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' /> }
+        {isAuth && <Redirect from='/login' to='/home' /> }
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path="/favs" />
-              <NotRegisteredUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
-
       <NavBar />
     </div>
   );
