b43f09ad6c9b05e32fa55fb7280177c5da85bc8f
Gabriel Gomez <gabriel70@gmail.com>
2020-10-10 22:48:12 -0300

    service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
 module.exports = {
   output: {
     filename: 'app.bundle.js',
@@ -7,6 +11,37 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new webpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      name: 'Petgram',
+      description: 'Con Petgran puedes encontrar fotos de animales domesticos adorables y muy amados',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            size: [96,128,192,256,184,512]
+          }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://res.cloudinary.com | image.unsplash.com'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-gabys-nomreb8hx.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
